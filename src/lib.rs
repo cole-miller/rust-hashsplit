7eae8691e99ac28cfb5faa78005b93c1e56cf92b
Cole Miller <git@cole-miller.net>
2020-08-18 17:28:33 -0400

    First pass at a generic interface and rrs impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,97 @@
+pub trait Hasher {
+    type Checksum;
+    type State;
+
+    fn width(&self) -> usize;
+    fn empty_checksum() -> Self::Checksum;
+    fn initial_state() -> Self::State;
+    fn process_byte(
+        &self,
+        state: &Self::State,
+        old_byte: u8,
+        new_byte: u8,
+    ) -> (Self::Checksum, Self::State);
+}
+
+pub struct Rolling<H, I>
+where
+    H: Hasher,
+{
+    hasher: H,
+    next: Option<H::Checksum>,
+    state: H::State,
+    begin: usize,
+    ring: Vec<u8>,
+    bytes: I,
+}
+
+impl<H, I> Rolling<H, I>
+where
+    H: Hasher,
+    I: Iterator<Item = u8>,
+{
+    pub fn start(hasher: H, mut it: I) -> Option<Self> {
+        let mut hold = (H::empty_checksum(), H::initial_state());
+        let mut i = 0;
+        let mut ring = Vec::with_capacity(hasher.width());
+
+        while let Some(byte) = it.next() {
+            if i == hasher.width() {
+                break;
+            }
+
+            let (_, state) = hold;
+            hold = hasher.process_byte(&state, 0, byte);
+            ring.push(byte);
+            i += 1;
+        }
+        let (sum, state) = hold;
+
+        if i < hasher.width() {
+            None
+        } else {
+            Some(Self {
+                hasher,
+                next: Some(sum),
+                state,
+                begin: 0,
+                ring,
+                bytes: it,
+            })
+        }
+    }
+
+    fn feed(&mut self, byte: u8) -> H::Checksum {
+        let (sum, new_state) = self
+            .hasher
+            .process_byte(&self.state, self.ring[self.begin], byte);
+        self.state = new_state;
+        self.ring[self.begin] = byte;
+        self.begin = (self.begin + 1) % self.hasher.width();
+
+        sum
+    }
+}
+
+impl<H, I> Iterator for Rolling<H, I>
+where
+    H: Hasher,
+    I: Iterator<Item = u8>,
+{
+    type Item = H::Checksum;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut new_next = if let Some(byte) = self.bytes.next() {
+            Some(self.feed(byte))
+        } else {
+            None
+        };
+
+        std::mem::swap(&mut self.next, &mut new_next);
+        let old_next = new_next;
+
+        old_next
+    }
+}
+
 pub mod rrs;
--- src/rrs.rs
+++ src/rrs.rs
@@ -1,13 +1,11 @@
-#[allow(dead_code)]
+pub type Checksum = u32;
+pub type State = (u32, u32);
 
 pub struct Hasher {
     modulus: u32,
     offset: u32,
     width: usize,
 }
-
-pub type Checksum = u32;
-pub type State = (u32, u32);
 
 impl Hasher {
     pub fn new(modulus: u32, offset: u32, width: usize) -> Self {
@@ -17,16 +15,25 @@ impl Hasher {
             width,
         }
     }
+}
 
-    pub fn initial_state() -> State {
-        (0, 0)
-    }
+impl crate::Hasher for Hasher {
+    type Checksum = Checksum;
+    type State = State;
 
-    pub fn width(&self) -> usize {
+    fn width(&self) -> usize {
         self.width
     }
 
-    pub fn process_byte(&self, state: State, old_byte: u8, new_byte: u8) -> (Checksum, State) {
+    fn empty_checksum() -> Checksum {
+        0
+    }
+
+    fn initial_state() -> State {
+        (0, 0)
+    }
+
+    fn process_byte(&self, state: &State, old_byte: u8, new_byte: u8) -> (Checksum, State) {
         let (a, b) = state;
         let a_new = (a - old_byte as u32 + new_byte as u32) % self.modulus;
         let b_new =
@@ -35,89 +42,4 @@ impl Hasher {
 
         (sum, (a_new, b_new))
     }
-
-    pub fn roll<I>(self, it: I) -> Rolling<I>
-    where
-        I: Iterator<Item = u8>,
-    {
-        Rolling::start(self, it)
-    }
-}
-
-pub struct Rolling<I> {
-    hasher: Hasher,
-    next: Option<Checksum>,
-    state: State,
-    begin: usize,
-    ring: Vec<u8>,
-    bytes: I,
-}
-
-impl<I> Rolling<I>
-where
-    I: Iterator<Item = u8>,
-{
-    fn start(hasher: Hasher, mut it: I) -> Self {
-        let mut hold = (0, Hasher::initial_state()); // XXX
-        let mut i = 0;
-        let mut ring = Vec::with_capacity(hasher.width);
-
-        while let Some(byte) = it.next() {
-            if i == hasher.width() {
-                break;
-            }
-
-            let (_, state) = hold;
-            hold = hasher.process_byte(state, 0, byte);
-            ring.push(byte);
-            i += 1;
-        }
-        let (sum, state) = hold;
-
-        if i < hasher.width() {
-            panic!("you didn't give me enough bytes!");
-        }
-
-        Self {
-            hasher,
-            next: Some(sum),
-            state,
-            begin: 0,
-            ring,
-            bytes: it,
-        }
-    }
-
-    fn feed(&mut self, byte: u8) {
-        let (sum, state_new) = self
-            .hasher
-            .process_byte(self.state, self.ring[self.begin], byte);
-        self.next = Some(sum);
-        self.state = state_new;
-        self.ring[self.begin] = byte;
-        self.begin = (self.begin + 1) % self.hasher.width();
-    }
-}
-
-impl<I> Iterator for Rolling<I>
-where
-    I: Iterator<Item = u8>,
-{
-    type Item = Checksum;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let saved = self.next;
-
-        if let Some(byte) = self.bytes.next() {
-            self.feed(byte);
-        } else {
-            self.next = None;
-        }
-
-        saved
-    }
-}
-
-pub fn rrs1(width: usize) -> Hasher {
-    Hasher::new(1 << 16, 31, width)
 }
