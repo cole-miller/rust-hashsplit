9db5675e6651c34919c680657d5a4d8a6f5c36f2
Cole Miller <git@cole-miller.net>
2020-08-18 18:24:24 -0400

    Implement the rrs0/rrs1 split

--- src/rrs.rs
+++ src/rrs.rs
@@ -1,17 +1,24 @@
 pub type Checksum = u32;
 pub type State = (u32, u32);
 
+pub enum Style {
+    Rrs0,
+    Rrs1,
+}
+
 pub struct Hasher {
     modulus: u32,
     offset: u32,
+    style: Style,
     width: usize,
 }
 
 impl Hasher {
-    pub fn new(modulus: u32, offset: u32, width: usize) -> Self {
+    pub fn new(modulus: u32, offset: u32, style: Style, width: usize) -> Self {
         Self {
             modulus,
             offset,
+            style,
             width,
         }
     }
@@ -38,8 +45,20 @@ impl crate::Hasher for Hasher {
         let a_new = (a - old_byte as u32 + new_byte as u32) % self.modulus;
         let b_new =
             (b - self.width as u32 * (old_byte as u32 + self.offset) + a_new) % self.modulus;
-        let sum = b_new + (a_new << 16);
+
+        let sum = match self.style {
+            Style::Rrs0 => a_new + (b_new << 16),
+            Style::Rrs1 => b_new + (a_new << 16),
+        };
 
         (sum, (a_new, b_new))
     }
+}
+
+pub fn rrs0(width: usize) -> Hasher {
+    Hasher::new(1 << 16, 31, Style::Rrs0, width)
+}
+
+pub fn rrs1(width: usize) -> Hasher {
+    Hasher::new(1 << 16, 31, Style::Rrs1, width)
 }
