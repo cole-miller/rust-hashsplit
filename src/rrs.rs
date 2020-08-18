fdbdfc253fe783a89f82077d2d27427900b20aca
Cole Miller <git@cole-miller.net>
2020-08-18 16:44:23 -0400

    Fix math error in rrs calculation
    This corresponds to the fix in hashsplit-spec/pull/2.

--- src/rrs.rs
+++ src/rrs.rs
@@ -29,7 +29,8 @@ impl Hasher {
     pub fn process_byte(&self, state: State, old_byte: u8, new_byte: u8) -> (Checksum, State) {
         let (a, b) = state;
         let a_new = (a - old_byte as u32 + new_byte as u32) % self.modulus;
-        let b_new = (b - self.width as u32 * (old_byte as u32 + a_new)) % self.modulus;
+        let b_new =
+            (b - self.width as u32 * (old_byte as u32 + self.offset) + a_new) % self.modulus;
         let sum = b_new + (a_new << 16);
 
         (sum, (a_new, b_new))
